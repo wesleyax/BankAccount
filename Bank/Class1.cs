61f01ff59f368021c260fcd68cb3f0a59797c7e3
wesleyax <61523905+wesleyax@users.noreply.github.com>
2024-12-03 18:03:32 +1300

    Add project files.

---
+++ Bank/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Bank {
+    internal class Class1 {
+        //--------------------------------- PRIVATE ATTRIBUTES ----------------------------------\\
+        private string _name;
+
+        //----------------------------- AUTO-IMPLEMENTED PROPERTIES -----------------------------\\
+        public int AccountNumber { get; private set; }
+        public double Deposit { get; private set; }
+
+        //------------------------------------- CONSTRUCTORS ------------------------------------\\
+
+        public Class1(int accountNumber, string name) {
+            AccountNumber = accountNumber;
+            _name = name;
+            //Deposit = 0;
+        }
+
+        public Class1(int accountNumber, string name, double deposit) : this (accountNumber, name) {
+            AddDeposit(deposit);
+        }
+
+        //---------------------------------- CUSTOM PROPERTIES ----------------------------------\\
+        public string Name {
+            get { return _name; }
+            set {
+                if (value != null && value.Length > 4) {
+                    _name = value;
+                }
+            }
+        }
+
+        //----------------------------- OTHER METHODS OF THE CLASS ------------------------------\\
+        public override string ToString() {
+            return "Account number: " + AccountNumber + ", Account name: " + _name + ", Balance: " + Deposit.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public void AddDeposit(double amount) {
+            Deposit += amount;
+        }
+        public void Withdrawal(double amount) {
+            Deposit -= amount + 5.0;
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = 0.0;
+
+            Console.Write("Account number: ");
+            int account = int.Parse(Console.ReadLine());
+
+            Console.Write("Account holder name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Will there be an initial deposit (y/n)?: ");
+            string option = Console.ReadLine();
+
+            if (option.ToLower() == "y") {
+                Console.Write("Enter the initial deposit amount: $");
+                deposit = double.Parse(Console.ReadLine());
+            } 
+
+            Class1 class1 = new Class1(account, name, deposit);
+
+            Console.Clear();
+            Console.WriteLine("Account details:\r\n" + class1);
+
+            Console.Write("Enter a deposit amount: ");
+            deposit = double.Parse(Console.ReadLine());
+            class1.AddDeposit(deposit);
+
+            Console.Clear();
+            Console.WriteLine("Updated account details:\r\n" + class1);
+
+            
+            Console.Write("Enter a withdrawal amount: ");
+            deposit = double.Parse(Console.ReadLine());
+            class1.Withdrawal(deposit);
+            
+            Console.Clear();
+            Console.WriteLine("Updated account details:\r\n" + class1);
+
+            Console.ReadKey();
+        }
+    }
+}
